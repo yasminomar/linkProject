44858d514fbd1a40ee91e999549058c6395cb79a
UGS <UGS.140342@ci.suez.edu.eg>
2022-07-20 01:14:42 +0200

    k

--- src/app/Modules/home/Components/Customer-home/customer.component.ts
+++ src/app/Modules/home/Components/Customer-home/customer.component.ts
@@ -53,7 +53,7 @@ export class CustomerComponent implements OnInit  {
     this.productInCartWriteDto.productId=e.id;
     this.productService.AddProductToCart(this.productInCartWriteDto).subscribe({
       next:(p)=>{
-        this.GetProducts();
+        this.fillUserCart();
       },
       error: (err) => {
         alert(err.error);
