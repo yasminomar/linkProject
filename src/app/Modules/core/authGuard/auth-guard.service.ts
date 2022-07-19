4b0ba2ef4252f03b089f45b42598778761046a5b
UGS <UGS.140342@ci.suez.edu.eg>
2022-07-19 18:29:30 +0200

    test customer2

--- src/app/Modules/core/authGuard/auth-guard.service.ts
+++ src/app/Modules/core/authGuard/auth-guard.service.ts
@@ -13,7 +13,13 @@ export class AuthGuardService implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {   
-    if(this.auth.isLoggedIn()) return true;
+    if(this.auth.isLoggedIn()){
+      const token = this.auth.getToken();
+      const tokenInfo = this.auth.getDecodedAccessToken(token!);
+        if(tokenInfo["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] == "Customer"){
+            return true;
+        }
+    }
     this.router.navigate(['/auth/login']);
      return false;
   }
